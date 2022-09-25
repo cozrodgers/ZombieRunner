ada2dc0e816936fc3982730c3be388604e8a8b34
cozrodgers <coreyrodgers@gmail.com>
2022-09-25 12:07:31 +1000

    add muzzle flash fx

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,10 +8,12 @@ public class Weapon : MonoBehaviour
 
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         muzzleFlash = GetComponentInChildren<ParticleSystem>();
     }
     void Update()
@@ -39,6 +41,7 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
+            CreateHitImpact(hit);
 
             Debug.Log("I hit this thing" + hit.transform.name);
             //TODO add hit FX
@@ -54,5 +57,13 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+
+        GameObject fx = Instantiate(hitEffect, hit.point, Quaternion.identity);
+        Destroy(fx, 0.2f);
+        
     }
 }
