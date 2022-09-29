5f6dbd539c41bd9f1cd10627a8ce4117ef76eff1
cozrodgers <coreyrodgers@gmail.com>
2022-09-30 06:28:17 +1000

    add battery pickup sfx

--- Assets/Scripts/BatterPickup.cs
+++ Assets/Scripts/BatterPickup.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class BatterPickup : MonoBehaviour
 {
     [SerializeField] float chargeAmount = 10f;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip chargeSFX;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            audioSource = other.GetComponent<AudioSource>();
+
             if (gameObject)
             {
                 Destroy(gameObject, 0.2f);
-
             }
 
             other.GetComponentInChildren<FlashlightSystem>().IncreaseBattery(chargeAmount);
-
         }
     }
+
+    void PlayChargeSFX()
+    {
+       audioSource.PlayOneShot(chargeSFX);
+    }
+
+
 }
